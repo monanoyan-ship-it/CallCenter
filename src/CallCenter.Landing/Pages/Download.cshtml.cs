1d5b7980f679d4d7507028419ab156eb4f4c3ff9
ahmet özgüç <mona.noyan@gmail.com>
2026-03-09 07:18:20 +0300

    uygulama download

---
+++ src/CallCenter.Landing/Pages/Download.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CallCenter.Landing.Pages;
+
+public class DownloadModel : PageModel
+{
+    public string WindowsVersion { get; set; } = "0.1.0";
+    public string WindowsDownloadUrl { get; set; } = "https://storage.googleapis.com/corplynk-releases/CorpLynk-0.1.0-setup.exe";
+
+    public void OnGet()
+    {
+    }
+}
