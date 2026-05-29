35cfb0f820f470af8b613a6e8cda46b0710936de
ahmet özgüç <mona.noyan@gmail.com>
2026-05-29 18:13:03 +0300

    Release Windows app 0.4.7 metadata

--- src/CallCenter.Landing/Pages/Download.cshtml.cs
+++ src/CallCenter.Landing/Pages/Download.cshtml.cs
@@ -4,8 +4,8 @@ namespace CallCenter.Landing.Pages;
 
 public class DownloadModel : PageModel
 {
-    public string WindowsVersion { get; set; } = "0.4.6";
-    public string WindowsDownloadUrl { get; set; } = "https://storage.googleapis.com/corplynk-releases/CorpLynk-0.4.6-setup.exe";
+    public string WindowsVersion { get; set; } = "0.4.7";
+    public string WindowsDownloadUrl { get; set; } = "https://storage.googleapis.com/corplynk-releases/CorpLynk-0.4.7-setup.exe";
 
     public void OnGet()
     {
